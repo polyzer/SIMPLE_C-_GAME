ae8873c41165a0ba1a78e79e64f064af6908365f
polyzer <realpolyzer@yandex.ru>
2015-05-02 08:51:02 +0300

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -3,30 +3,41 @@
 #include <conio.h>
 #include <ctime>
 
-#define h_MatrixSize_i 8
-#define v_MatrixSize_i 8
 
-HANDLE hConsole;
 
-void goToXY(int col, int row) {
-	
-	COORD coord;
-	coord.X = col;
-	coord.Y = row;
+#define h_MatrixSize_i 8 // horizontal size
+#define v_MatrixSize_i 8 // vertical size
+char current_PATH_ca[255];
+DWORD buf_length = 255;
 
-	HANDLE hConsoleComeTo = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleCursorPosition(hConsoleComeTo, coord);
+
+HANDLE hConsole; //console handler
+
+void getCurrentPath(){
+	GetCurrentDirectoryA(buf_length, current_PATH_ca);
 
 }
 
-struct hero {
+void goToXY(int col, int row) { // come to position
+	
+	COORD coord; //coordinate
+	coord.X = col;
+	coord.Y = row;
 
-	char face;
+	HANDLE hConsoleComeTo = GetStdHandle(STD_OUTPUT_HANDLE); // catch a handler
+	SetConsoleCursorPosition(hConsoleComeTo, coord); //set cursor's position
+
+}
+
+struct hero { //our manipulator
+
+	char face; //it will be shown
 	int X, Y;
 
 } man;
 
-char boardMatrix[h_MatrixSize_i][v_MatrixSize_i] = 
+
+char boardMatrix[h_MatrixSize_i][v_MatrixSize_i] = // this is our placement
 {
 	{100, 100, 100, 100, 100, 100, 100, 100},
 	{100, 100, 100, 100, 100, 100, 100, 100},
@@ -41,8 +52,8 @@ char boardMatrix[h_MatrixSize_i][v_MatrixSize_i] =
 
 //char keyPressed;
 
-void render() { //функция рисования
-	goToXY(0,0);
+void render() { //our painter
+	goToXY(0,0);// at first - come to 0,0
 	for (int i=0; i<h_MatrixSize_i; i++)
 	{
 		for (int j=0; j<v_MatrixSize_i; j++) 
@@ -63,8 +74,16 @@ void render() { //функция рисования
 
 int main ( int argc, char **argv[]) 
 {
+	setlocale(LC_CTYPE, "RUS");
+
 	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hConsole, 14);
+
+	getCurrentPath();
+	printf("%s", current_PATH_ca);
+	system("pause");
+
+	system("cls");
 
 	man.face = 110;
 	man.X = 0;
@@ -72,26 +91,44 @@ int main ( int argc, char **argv[])
 	//char readSymbol = 'd';
 	int IR1 = 0;
 	int IR2 = 0;
-	while (true) {		
 		render();
+	while (true) {		
+//		render();
 		if (_kbhit()) {
 			IR1 = _getch();
 			if (IR1 == 224) {
 				IR2 = _getch();
 				if (IR2 == 75) {
-					man.X--;
+					if (man.X > 0)
+					{
+						man.X--;
+						render();	
+					}
+					
 					continue;
 				}
 				if (IR2 == 77) {
-					man.X++;
+					if (man.X < (h_MatrixSize_i - 1))
+					{
+						man.X++;
+						render();
+					}
 					continue;
 				}
 				if (IR2 == 72) {
-					man.Y--;
+					if (man.Y > 0) 
+					{
+						man.Y--;
+						render();
+					}
 					continue;
 				}
 				if (IR2 == 80) {
-					man.Y++;	
+					if (man.Y < (v_MatrixSize_i - 1)) 
+					{
+						man.Y++;	
+						render();
+					}
 					continue;
 				}
 			}
